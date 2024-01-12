b93426eb706524336a7254d4aafa3f3d1efb2009
Vladislav <120631119+VladosusPing@users.noreply.github.com>
2024-01-12 12:28:18 +0000

    add urls.py run_script

--- web/HoMagusWeb/HoMagusWeb/urls.py
+++ web/HoMagusWeb/HoMagusWeb/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import run_script
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('run_script/', run_script, name='run_script'),
 ]
