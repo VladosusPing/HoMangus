da8f425074a1c6d6e297f6ae22dc6dbb62bfecd5
Vladislav <120631119+VladosusPing@users.noreply.github.com>
2024-01-12 12:26:13 +0000

    add views.py and

---
+++ web/HoMagusWeb/HoMagusWeb/views.py
@@ -0,0 +1,18 @@
+# views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+import subprocess
+
+def run_script(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('user_input', '')  # Получаем значение из формы
+        script_path = '/путь/к/твоему/скрипту.py'
+        arguments = [user_input]
+
+        result = subprocess.run(['python', script_path] + arguments, capture_output=True)
+        print(result.stdout.decode('utf-8'))
+
+        return HttpResponse("Скрипт успешно запущен с аргументом: {}".format(user_input))
+    return render(request, 'your_app/your_template.html')
+   
+
